bf5756e94ed83abf0639de7ceddd1adad8e0cc5f
Jill Reese <ajreese@gmail.com>
2022-12-22 15:26:03 -0500

    New program.cs file

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myString = "Blue Dog";
+            int myInt = 29;
+            char myChar = 'J';
+            bool myStatus = false;
+            double myDouble = 13.5;
+            decimal myDecimal = 57.89m;
+
+            Console.WriteLine($"The {myString} ran {myInt} miles to jump on the {myChar}.");
+            Console.WriteLine($"I wondered if the story was {myStatus}, but the double was {myDouble}.");
+            Console.WriteLine($"It wasn't {myStatus} --the decimal was {myDecimal}.");
+
         }
     }
 }
